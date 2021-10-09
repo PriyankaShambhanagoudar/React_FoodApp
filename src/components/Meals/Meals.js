167e1b332eb460dae2ba8420a5986e1589906d35
React_Teaching_Project <priyassss789@gmail.com>
2021-10-09 14:08:54 +0530

    Adding Meals Components

---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,14 @@
+import { Fragment } from "react";
+
+import MealsSummary from "./MealsSummary";
+import AvilableMeals from "./AvailableMeals";
+
+const Meals = () => {
+  return (
+    <Fragment>
+      <MealsSummary />
+      <AvilableMeals />
+    </Fragment>
+  );
+};
+export default Meals;
